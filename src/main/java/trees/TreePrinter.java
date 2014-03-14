2f62c07c5397e656a54c4a71f0c61991be21723d
trequartista10 <mailkuldeepm@gmail.com>
2014-03-14 00:13:28 -0400

    Committing basic project structure. Few implementations on Binary Trees, LCS, Edit Distance, Prime Number Generators

--- src/main/java/trees/TreePrinter.java
+++ src/main/java/trees/TreePrinter.java
@@ -9,6 +9,15 @@ package trees;
  */
 public class TreePrinter {
 
+    public static <T extends Comparable<T>> void printTree(TreeNode<T> tree) {
+        int height = tree.getHeightAndDiameter()[0];
+        int totalPossibleNodes = (int)(Math.pow(2,height)) - 1;
+        //For height h there are 2^N nodes    there are n/(2^(h + 1) nodes
+        int noOfNodes = totalPossibleNodes / (int)Math.pow(2,height + 1) ;
+    }
 
+    public static void main(String[] args) {
+        System.out.println("Prints a Tree top down");
+    }
 
 }
