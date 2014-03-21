558483cda0f94ad9ddb0ffc398455a11d92607f2
trequartista10 <mailkuldeepm@gmail.com>
2014-03-20 23:28:01 -0400

    Committing basic project structure. Few implementations on Binary Trees, LCS, Edit Distance, Prime Number Generators

--- src/main/java/dp/LongestIncreasingSubSeq.java
+++ src/main/java/dp/LongestIncreasingSubSeq.java
@@ -8,4 +8,39 @@ package dp;
  * To change this template use File | Settings | File Templates.
  */
 public class LongestIncreasingSubSeq {
+
+    static void findLongestIncSubSeq(int[] numbers) {
+        int[] seqCounts = new int[numbers.length];
+        int allmax =0;
+        for(int i=0; i < numbers.length; i++) {
+            int max = numbers[i];
+            int cnt =1;
+            for(int j=i-1; j>=0; j--) {
+                if(numbers[j] < max) {
+                    max = numbers[j];
+                    cnt++;
+                }
+            }
+
+            seqCounts[i] = cnt;
+            if(seqCounts[i] > allmax)
+                allmax = seqCounts[i];
+        }
+        for(int i=0; i < numbers.length; i++)
+            System.out.print(numbers[i]+ " ");
+        System.out.println("\n" +allmax);
+        for(int k=numbers.length -1; k>=0; k-- ) {
+            if(seqCounts[k] == allmax) {
+                System.out.print(numbers[k] + " ");
+                allmax--;
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {0,4,1,2,8,11,12,6};
+        findLongestIncSubSeq(numbers);
+
+    }
 }
